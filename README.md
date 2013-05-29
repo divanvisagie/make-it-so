29a21c15c36d7412603145e81853469ad9c5fec2
Divan Visagie <visagiedivan@gmail.com>
2013-05-29 14:44:28 +0200

    refactor and readme update

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 Make it so
 ==========
 
-
 Watcher that runs make on file changes
+
+### Switches:
+
+	--ignore [files,]			ignores listed files
+
+	--ignore-dot-files			ignores dot files
--- index.js
+++ index.js
@@ -3,23 +3,26 @@
 var watch = require( 'watch' ),
 	colors = require( 'colors' ),
 	spawn = require( 'child_process' ).spawn,
-  path = require( 'path' );
+    path = require( 'path' );
 
-var args = process.argv.slice(2);
-var ignoreDotFiles = args.indexOf("--ignore-dot-files") >= 0;
-var ignores = args.reduce(function(state, arg) {
-  return {
-    ignores: state.include ? state.ignores.concat(arg) : state.ignores,
-    include: arg === "--ignore"
-  };
-}, { ignores: [], include: false }).ignores.map(function(relative) {
+/* parse the ignore arguments */
+var args = process.argv.slice( 2 );
+var ignoreDotFiles = args.indexOf( '--ignore-dot-files' ) >= 0;
+var ignores = args.reduce( function( state, arg ) {
+    return {
+        ignores : state.include ? state.ignores.concat( arg ) : state.ignores,
+        include : arg === '--ignore'
+    };
+}, { ignores: [], include: false }).ignores.map( function( relative ) {
   return path.resolve(relative);
 });
 
 console.log( 'watching root:' , process.cwd() );
-if (ignores.length) console.log( 'ignoring: ' , ignores.join( ',' ) )
+if ( ignores.length ) {
+    console.log( 'ignoring: ' , ignores.join( ',' ) );
+}
 
-function change_handler(f){
+function change_handler( f ){
   console.log( 'file changed running make:', f );
 	var ps = spawn( 'make' );
 
@@ -40,26 +43,25 @@ function change_handler(f){
 }
 
 watch.createMonitor( process.cwd(), {
-  ignoreDotFiles: ignoreDotFiles,
-  filter: function(path) {
-    //console.log(path)
-    return ignores.indexOf(path) >= 0
-  },
+
+    ignoreDotFiles: ignoreDotFiles,
+    filter: function( path ) {
+
+        return ignores.indexOf( path ) >= 0;
+    }
+
 }, function( monitor ){
 
-
-	monitor.on("created", function (f, stat) {
-      // Handle new files
-      change_handler(f);
+	monitor.on( 'created' , function ( f, stat ) {
+        // Handle new files
+        change_handler( f );
     });
-    monitor.on("changed", function (f, curr, prev) {
-      // Handle file changes
-      change_handler(f);
-
+    monitor.on( 'changed' , function ( f, curr, prev ) {
+        // Handle file changes
+        change_handler( f );
     });
-    monitor.on("removed", function (f, stat) {
-      // Handle removed files
-       change_handler(f);
-
+    monitor.on( 'removed' , function ( f, stat ) {
+        // Handle removed files
+        change_handler( f );
     });
 });
